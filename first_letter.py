faee250c3fd07ebdd3a7925a4c64321ef9cb2507
gyonghwan <gyonghwan@gmail.com>
2016-02-16 22:24:25 +0000

    This is first assignment script

---
+++ first_letter.py
@@ -0,0 +1,9 @@
+inputString = raw_input("Enter your password:") 
+
+ChangeString = inputString[0].upper()
+
+print "Your password with First Character with uppercase is ", ChangeString + inputString[1:]
+
+
+
+
