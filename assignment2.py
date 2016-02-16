cfc4fcdef61c5898fed1d89e2d41dc7fc090bd00
gyonghwan <gyonghwan@users.noreply.github.com>
2016-02-16 15:14:29 -0500

    Delete assignment2.py

--- assignment2.py
+++
@@ -1,7 +0,0 @@
-base = raw_input("Enter a base number: ")
-exponent = raw_input ("Enter an exponent: ")
-
-calc = int(base) ** int(exponent)
-
-print "{}to the power of{} = {}".format(base, exponent,calc) 
-
